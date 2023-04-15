57978e5c55be2c7d1a9b6e948993bfe4cc8cd11a
wendy barrios <wbarrios45@yahoo.com>
2023-04-15 13:28:59 -0700

    added clearField function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
 document.querySelector('.start-btn').addEventListener('click', getFetch)
 
+  //Start of function to clear out data from input form 
+  function clearField() {
+    document.querySelector("input").value = ""
+  }
+  //End of function to clear out data from input form 
+
 function getFetch(){
+
   const charactername = document.querySelector('input').value
   const url = `https://supermario-api-1.herokuapp.com/api/${charactername}`
 
@@ -10,6 +17,7 @@ function getFetch(){
       .then(data => {
         console.log(data)
         document.querySelector('.funfact').innerText = data.funFact
+        clearField()
 
       })
       .catch(err => {
